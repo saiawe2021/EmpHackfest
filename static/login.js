0b011417ef61885959fec7d34e488dca2370d977
MichaelFine2022 <mf9947967@gmail.com>
2024-09-07 12:40:37 -0700

    login page

---
+++ static/login.js
@@ -0,0 +1,5 @@
+let username = document.getElementById("userNameField");
+let password = document.getElementById("passwordField");
+username.addEventListener("click", function(){
+    alert("Login successful")
+})
