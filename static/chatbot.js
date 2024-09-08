2875d650ac1e2cee354f43189aea26af851802d3
MichaelFine2022 <mf9947967@gmail.com>
2024-09-08 10:29:16 -0700

    styled it and made the chatbot work

--- static/chatbot.js
+++ static/chatbot.js
@@ -1,35 +1,23 @@
 
-let planList = document.getElementById("planList");
-let answer;
-fetch("/post/AIcall")
-.then(response=>response.json())
-.then(result => console.log(result));
-
-
 //creates an event listener on the send button
 let sendBtn = document.getElementById(`sendButton`)
 if(sendBtn){
     sendBtn.addEventListener("click", function(){
         //gets and resets the sent text
         let sentText = document.getElementById(`typedText`).value
-        alert(`Sent text is ${sentText}`)
         document.getElementById(`typedText`).value = "";
         //creates a text node using that text
         let node = document.createTextNode(sentText)
-        alert(`The node is ${node}`)
         //creates a div with the right class
         let newDiv = document.createElement("div")
         newDiv.setAttribute("class", "outgoing-chats-msg")
-        alert(`new Div is ${newDiv}`)
         //creates a new paragraph and appends the node to it
         let newElement = document.createElement("p")         
         newElement.appendChild(node)
-        alert(`new Element is ${newElement}`)
         //appends the p element to the div 
         newDiv.appendChild(newElement)
         //gets the div to append to
         let element = document.getElementById(`outgoingMessageDirectory`)
-        alert(`Element is ${element}`)
         //appends the div with the paragraph to the outgoing chat messages div 
         element.appendChild(newDiv)
     })
